62a5260ba21871b4a1ae0b33576acf0e67252cc7
RiccardoImperiale <riccardoimperiale.dev@gmail.com>
2024-05-13 12:47:28 +0200

    move trains array to an external config file

---
+++ config/db.php
@@ -0,0 +1,42 @@
+<?php
+
+return [
+    'trains' => [
+        [
+            'company' => 'Trenitalia',
+            'departure_station' => 'Milano Centrale',
+            'arrival_station' => 'Varese Nord',
+            'departure_date' => '2024-05-01',
+            'departure_time' => '14:25:00',
+            'arrival_time' => '15:40:00',
+            'train_code' => '243577',
+            'carriages_number' => 9,
+            'in_time' => 0,
+            'cancelled' => 1,
+        ],
+        [
+            'company' => 'National Rail',
+            'departure_station' => 'Fairbourne',
+            'arrival_station' => 'Eaglescliffe',
+            'departure_date' => '2024-05-31',
+            'departure_time' => '15:03:00',
+            'arrival_time' => '22:10:00',
+            'train_code' => '764876',
+            'carriages_number' => 8,
+            'in_time' => 1,
+            'cancelled' => 0,
+        ],
+        [
+            'company' => 'Trenord',
+            'departure_station' => 'Tradate',
+            'arrival_station' => 'Laveno Nord',
+            'departure_date' => '2024-06-29',
+            'departure_time' => '09:02:00',
+            'arrival_time' => '10:00:00',
+            'train_code' => '19384',
+            'carriages_number' => 6,
+            'in_time' => 0,
+            'cancelled' => 0,
+        ]
+    ]
+];
--- database/seeders/TrainsTableSeeder.php
+++ database/seeders/TrainsTableSeeder.php
@@ -13,57 +13,7 @@ class TrainsTableSeeder extends Seeder
      */
     public function run(): void
     {
-        // $table->id();
-        // $table->string('company', 255)->nullable();
-        // $table->string('departure_station', 50);
-        // $table->string('arrival_station', 50);
-        // $table->time('departure_time', 0);
-        // $table->time('arrival_time', 0);
-        // $table->unsignedMediumInteger('train_code');
-        // $table->unsignedTinyInteger('carriages_number')->nullable();
-        // $table->boolean('in_time')->nullable();
-        // $table->boolean('cancelled')->nullable();
-        // $table->timestamps();
-
-        $trains = [
-            [
-                'company' => 'Trenitalia',
-                'departure_station' => 'Milano Centrale',
-                'arrival_station' => 'Varese Nord',
-                'departure_date' => '2024-05-01',
-                'departure_time' => '14:25:00',
-                'arrival_time' => '15:40:00',
-                'train_code' => '243577',
-                'carriages_number' => 9,
-                'in_time' => 0,
-                'cancelled' => 1,
-            ],
-            [
-                'company' => 'National Rail',
-                'departure_station' => 'Fairbourne',
-                'arrival_station' => 'Eaglescliffe',
-                'departure_date' => '2024-05-31',
-                'departure_time' => '15:03:00',
-                'arrival_time' => '22:10:00',
-                'train_code' => '764876',
-                'carriages_number' => 8,
-                'in_time' => 1,
-                'cancelled' => 0,
-            ],
-            [
-                'company' => 'Trenord',
-                'departure_station' => 'Tradate',
-                'arrival_station' => 'Laveno Nord',
-                'departure_date' => '2024-06-29',
-                'departure_time' => '09:02:00',
-                'arrival_time' => '10:00:00',
-                'train_code' => '19384',
-                'carriages_number' => 6,
-                'in_time' => 0,
-                'cancelled' => 0,
-            ],
-
-        ];
+        $trains = config('db.trains');
 
         foreach ($trains as $train) {
             $newTrain = new Train();
